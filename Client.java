90f049f793f6623da88c5a62ec87ef2325ffa57a
costmelot <109764105+costmelot@users.noreply.github.com>
2024-02-25 13:26:13 +0400

    Added

---
+++ Client.java
@@ -0,0 +1,39 @@
+package org.example.homework5;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        final Socket client = new Socket("localhost", Server.PORT);
+        new Thread(() -> {
+           try (Scanner input = new Scanner(client.getInputStream())) {
+               while (true) {
+                   System.out.println(input.nextLine());
+               }
+           } catch (IOException e) {
+               throw new RuntimeException(e);
+           }
+        }).start();
+
+        new Thread(() -> {
+           try(PrintWriter output = new PrintWriter(client.getOutputStream(), true)) {
+               Scanner consoleScanner = new Scanner(System.in);
+               while (true) {
+                   String consoleInput = consoleScanner.nextLine();
+                   output.println(consoleInput);
+                   if (Objects.equals("q", consoleInput)) {
+                       client.close();
+                       break;
+                   }
+               }
+           } catch (IOException e) {
+               throw new RuntimeException(e);
+           }
+        }).start();
+
+    }
+}
